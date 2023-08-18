b97c9da59156152fcb657aa3f44c7ed751116611
3S Cavalcante <aderc19@gmail.com>
2023-08-18 11:37:29 -0300

    Alterados métodos get() e set() para métodos 'mágicos' __get(), __set()

--- public/index.php
+++ public/index.php
@@ -5,4 +5,6 @@ require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
 print_r($user);
-echo 'Fim!';
+echo '<br>';
+$user->email = 'lucas_alterado@hotmail.com';
+print_r($user->email);
