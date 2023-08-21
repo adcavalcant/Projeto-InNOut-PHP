0512c4bc4ce02ca6b08fcedc900cc49200cf7db5
3S Cavalcante <aderc19@gmail.com>
2023-08-21 15:36:02 -0300

    Obtendo usuários do banco - função get()

--- public/index.php
+++ public/index.php
@@ -6,6 +6,20 @@ require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
 // echo $user->getSelect();
 
-echo User::getSelect(['id'=> 1], 'name, email');
+// echo User::getSelect(['id'=> 1], 'name, email');
+// echo '<br>';
+// echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
+
+print_r(User::get(['id' => 1], 'id, name, email'));
 echo '<br>';
-echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
+print_r(User::get(['name' => 'Chaves'], 'id, name, email'));
+echo '<br>';
+print_r(User::get([], 'name'));
+
+echo '<br>';
+echo '<br>';
+
+foreach(User::get([], 'name') as $user){
+    echo $user->name;
+    echo '<br>';
+};
