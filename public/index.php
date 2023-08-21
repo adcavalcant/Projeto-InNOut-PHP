9b963004c008032b82f6ed35388a5c8b36494d5d
3S Cavalcante <aderc19@gmail.com>
2023-08-21 16:57:06 -0300

    Tela de Login - front

--- public/index.php
+++ public/index.php
@@ -1,25 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-// echo $user->getSelect();
-
-// echo User::getSelect(['id'=> 1], 'name, email');
-// echo '<br>';
-// echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
-
-print_r(User::get(['id' => 1], 'id, name, email'));
-echo '<br>';
-print_r(User::get(['name' => 'Chaves'], 'id, name, email'));
-echo '<br>';
-print_r(User::get([], 'name'));
-
-echo '<br>';
-echo '<br>';
-
-foreach(User::get([], 'name') as $user){
-    echo $user->name;
-    echo '<br>';
-};
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
